fc3c42dc9a088249d738af2c37163aeec29d0aaa
Mischov <mmischov@gmail.com>
2017-03-24 17:04:08 -0500

    Add documentation to Meeseeks.TupleTree

--- lib/meeseeks/tuple_tree.ex
+++ lib/meeseeks/tuple_tree.ex
@@ -1,9 +1,27 @@
 defmodule Meeseeks.TupleTree do
-  @moduledoc false
+  @moduledoc """
+  HTML documents in Elixir/Erlang have traditionally been represented by a
+  tuple-tree like:
+
+  ```elixir
+  {"html", [], [
+    {"head", [], []}
+    {"body", [], [
+      {"h1", [{"id", "greeting"}], ["Hello, World!"]}]}]}
+  ```
+
+  `:mochiweb_html` parsed HTML into this format, and the tools for selecting
+  HTML used this format, so `html5ever` (the Elixir NIF) choose to output
+  to this format as well.
+
+  Meeseeks accepts tuple-trees as input, creating `Meeseeks.Document`s from
+  them.
+  """
 
   @type text :: String.t
   @type comment :: {:comment, String.t}
+  @type doctype :: {:doctype, String.t, String.t, String.t}
   @type element :: {String.t, [{String.t, String.t}], [node_t]}
-  @type node_t :: text | comment | element
+  @type node_t :: comment | doctype | element | text
   @type t :: node | [node_t]
 end
