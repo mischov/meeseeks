8b59b05f8431be598cbf28c42e139821feb8cac6
Mischov <mmischov@gmail.com>
2017-04-28 21:44:15 -0500

    Add new queries to Meeseeks.Document
      - Add `parent`, `ancestors`, and `previous_siblings` queries

--- test/meeseeks/document_test.exs
+++ test/meeseeks/document_test.exs
@@ -19,7 +19,23 @@ defmodule Meeseeks.DocumentTest do
     assert Document.element?(@document, 1)
   end
 
-  test "body > div children" do
+  test "parent" do
+    assert Document.parent(@document, 7) == 4
+  end
+
+  test "no parent" do
+    assert Document.parent(@document, 1) == nil
+  end
+
+  test "ancestors" do
+    assert Document.ancestors(@document, 7) == [4, 3, 1]
+  end
+
+  test "no ancestors" do
+    assert Document.ancestors(@document, 1) == []
+  end
+
+  test "children" do
     assert Document.children(@document, 4) == [5, 6, 7, 10]
   end
 
@@ -29,6 +45,10 @@ defmodule Meeseeks.DocumentTest do
 
   test "siblings" do
     assert Document.siblings(@document, 6) == [5, 6, 7, 10]
+  end
+
+  test "previous_siblings" do
+    assert Document.previous_siblings(@document, 7) == [5, 6]
   end
 
   test "next_siblings" do
