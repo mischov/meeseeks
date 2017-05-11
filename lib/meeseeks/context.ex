61db14a384a76b691206310b07e84c2bd5800092
Mischov <mmischov@gmail.com>
2017-05-10 19:22:35 -0500

    Add Meeseeks.Context

---
+++ lib/meeseeks/context.ex
@@ -0,0 +1,118 @@
+defmodule Meeseeks.Context do
+  @moduledoc """
+  Context is available to both Meeseek's selection process and each
+  individual selector, and allows for selectors to build state (or receive
+  state from the selection mechanism).
+
+  The selection process expects an `accumulator`, `return?` boolean, and
+  `matches` map to exist in the context, and stores selected nodes in the
+  `accumulator`, stores matching nodes than need to be filtered in the
+  `matches` map, and halts selection if the `return?` boolean becomes true.
+  """
+
+  alias Meeseeks.{Accumulator, Document, Selector}
+
+  @accumulator :"__accumulator__"
+  @return? :"__return?__"
+  @matches :"__matches__"
+  @nodes :"__nodes__"
+
+  @type t :: %{optional(any) => any}
+
+  @doc """
+  Creates a new context from an initial_context map.
+  """
+  @spec new(t) :: t
+  def new(initial_context) do
+    initial_context
+    |> Map.put(@return?, false)
+    |> Map.put(@matches, %{})
+  end
+
+  @doc """
+  Adds an accumulator to context, overriding any accumulator provided in the
+  initial context.
+  """
+  @spec with_accumulator(t, Accumulator.t) :: t
+  def with_accumulator(context, acc) do
+    Map.put(context, @accumulator, acc)
+  end
+
+  @doc """
+  Updates the context's accumulator with the result of calling
+  Accumulator.add on the current accumulator with the provided document and
+  id, and sets return? to the result of calling Accumulator.complete? on the
+  updated accumulator if return? was not already true.
+  """
+  @spec add_to_accumulator(t, Document.t, Document.node_id) :: t
+  def add_to_accumulator(%{@accumulator => acc, @return? => ret} = context, document, id) do
+    acc = Accumulator.add(acc, document, id)
+    ret = ret or Accumulator.complete?(acc)
+    %{context |
+      @accumulator => acc,
+      @return? => ret}
+  end
+
+  @doc """
+  Returns the result of calling Accumulator.return on the context's
+  accumulator.
+  """
+  @spec return_accumulator(t) :: any
+  def return_accumulator(%{@accumulator => acc}) do
+    Accumulator.return(acc)
+  end
+
+  @doc """
+  Adds a node to a list in the context's matches map corresponding to the
+  selector that the node matched.
+  """
+  @spec add_to_matches(t, Selector.t, Document.node_t) :: t
+  def add_to_matches(%{@matches => matches} = context, selector, node) do
+    case Map.fetch(matches, selector) do
+      {:ok, nodes} -> put_in(context[@matches][selector], [node|nodes])
+      :error -> put_in(context[@matches][selector], [node])
+    end
+  end
+
+  @doc """
+  Clears the context's matches map.
+  """
+  @spec clear_matches(t) :: t
+  def clear_matches(context) do
+    Map.put(context, @matches, %{})
+  end
+
+  @doc """
+  Returns the key under which the accumulator is stored in the context.
+  """
+  @spec accumulator_key() :: atom
+  def accumulator_key() do
+    @accumulator
+  end
+
+  @doc """
+  Returns the key under which return? is stored in the context.
+  """
+  @spec return_key() :: atom
+  def return_key() do
+    @return?
+  end
+
+  @doc """
+  Returns the key under which matching nodes that need to be filtered are
+  stored in the context.
+  """
+  @spec matches_key() :: atom
+  def matches_key() do
+    @matches
+  end
+
+  @doc """
+  Returns the key under which the nodes currently being walked are stored in
+  the context.
+  """
+  @spec nodes_key() :: atom
+  def nodes_key() do
+    @nodes
+  end
+end
